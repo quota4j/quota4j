d51e4efd3a196b01c2081457c9149e61be8840a6
Salvatore Novelli <salvatore.novelli@gmail.com>
2022-09-30 13:42:57 +0100

    minor refactoring

--- src/main/java/com/myseotoolbox/quota4j/model/ResourceQuota.java
+++ src/main/java/com/myseotoolbox/quota4j/model/ResourceQuota.java
@@ -3,4 +3,4 @@ package com.myseotoolbox.quota4j.model;
 
 import java.io.Serializable;
 
-public record ResourceQuota(String id, String quotaManagerClassName, Object initialState) implements Serializable { }
+public record ResourceQuota(String resourceId, String quotaManagerClassName, Object initialState) implements Serializable { }
